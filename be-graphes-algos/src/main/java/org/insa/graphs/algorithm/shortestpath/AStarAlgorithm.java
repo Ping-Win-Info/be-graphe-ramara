297891846a0b45a0e9966a7f04eb84c6ebda62b2
Ramara Matis <ramara@insa-20235>
2023-05-26 09:30:52 +0200

    A* marche en distance et rapidite

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.management.RuntimeErrorException;
 
+import org.insa.graphs.algorithm.AbstractInputData.Mode;
 import org.insa.graphs.algorithm.AbstractSolution.Status;
 import org.insa.graphs.algorithm.utils.BinaryHeap;
 import org.insa.graphs.model.Arc;
@@ -18,13 +19,20 @@ public class AStarAlgorithm extends DijkstraAlgorithm {
     public AStarAlgorithm(ShortestPathData data) {
         super(data);
     }
+
     
-    //NB:pourquoi ça override pas?
     protected void init(){
-        
+        int mode;
         int i =0;
+        int max_speed=graph.getGraphInformation().getMaximumSpeed();
+        if (data.getMode()==Mode.TIME) {
+            mode=0;
+        } else {
+            mode=1;
+        }
+
          for (Node node : graph.getNodes()) {//On parcours tout les sommets du graphe
-            labels[i]=new LabelStar(node, false, null,data.getDestination());//NB: on les mets tous avec un cout infini ( par défault )
+            labels[i]=new LabelStar(node, false, null,data.getDestination(),mode,max_speed);//NB: on les mets tous avec un cout infini ( par défault )
             i++;
          }
 
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/LabelStar.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/LabelStar.java
@@ -11,10 +11,22 @@ public class LabelStar extends Label{
    
    private Double cout_estime ;
    
-   public LabelStar(Node sommet, boolean marque, Arc père, Node dest) {
+   public LabelStar(Node sommet, boolean marque, Arc père, Node dest,int mode, int max_speed) { //maxspeed en km/h
 	   super(sommet,marque,père);
 	   this.cout = Double.MAX_VALUE;
-       this.cout_estime = sommet.getPoint().distanceTo(dest.getPoint());
+      double distance_estime;
+      double vitesse_ms ;
+      if (mode==0) { //On est en temps
+         distance_estime=sommet.getPoint().distanceTo(dest.getPoint());
+         
+         //v = d/t => t = d/v, d en mètre, v en m/s
+         vitesse_ms=max_speed/3.6;
+         this.cout_estime=distance_estime/vitesse_ms; 
+
+      } else {
+         this.cout_estime = sommet.getPoint().distanceTo(dest.getPoint());
+      }
+       
    }
    
    
